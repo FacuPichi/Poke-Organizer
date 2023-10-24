d123f0c674a4b59e9aec603186bedb488aecb0d9
Facundo Piccirillo <facupichi@hotmail.com>
2023-10-24 20:59:06 -0300

    feat: add Button that change the pokemon sprite

--- app/src/main/java/com/example/poke_organizer/MainActivity.java
+++ app/src/main/java/com/example/poke_organizer/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.poke_organizer;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.ProgressBar;
 import java.util.Random;
+import android.view.View;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -23,16 +25,16 @@ public class MainActivity extends AppCompatActivity {
         TextView level = findViewById(R.id.level);
         ImageView pokeSprite = findViewById(R.id.PokeSprite);
         ProgressBar progressBar = findViewById(R.id.progressBar); // Asegúrate de que el ID sea el correcto en tu XML.
-
+        Button pokemonChange = findViewById(R.id.pokemonChange);
         // Define el rango de números aleatorios que deseas
         int min = 1; // Valor mínimo
         int max = 1010; // Valor máximo
 
         // Genera un número aleatorio en el rango [min, max]
-        //int numeroAleatorio = random.nextInt(max - min + 1) + min;
+        int numeroAleatorio = random.nextInt(max - min + 1) + min;
 
         // numero pokedex para obtener el Pokémon
-        String relativeUrl = "pokemon/"+51+"/";
+        String relativeUrl = "pokemon/"+numeroAleatorio+"/";
         new GetPokemonInfo(pokename, pokeSprite).execute(relativeUrl);
 
         // establece el numero de nivel
@@ -42,5 +44,11 @@ public class MainActivity extends AppCompatActivity {
         progressBar.setIndeterminate(false);
         progressBar.setMax(100);
         progressBar.setProgress(progressValue);
+
+        pokemonChange.setOnClickListener(view -> {
+            int numeroAleatorio1 = random.nextInt(max - min + 1) + min;
+            String relativeUrl1 = "pokemon/" + numeroAleatorio1 + "/";
+            new GetPokemonInfo(pokename, pokeSprite).execute(relativeUrl1);
+        });
     }
 }
